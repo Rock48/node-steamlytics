83ee6e919f5aa1f2acd7a9a82afa04fefafa5a05
Rock48 <cquiglz@gmail.com>
2016-07-22 11:35:57 -0400

    Updated to pass errors to callbacks and put callbacks last in the arg list.
    This should not break your calls if you are not using extra parameters.

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # node-steamlytics
-A node-js module for http://steamlytics.xyz/
+A node.js module for http://steamlytics.xyz/
 
 You can install the package via npm using
 
 	npm install node-steamlytics --save
 
-How to use
------------
+## How to use
 
 It's extremely simple to use node-steamlytics. Every function corresponds with an equivalent API call. For example, if I want to get the price of an AWP | Dragon Lore (Factory New), it's as simple as:
 
@@ -14,8 +13,10 @@ It's extremely simple to use node-steamlytics. Every function corresponds with a
 
 	var SteamlyticsAPI = require("node-steamlytics").API;
 	var steamlytics = new SteamlyticsAPI("myAPIKeyHere", (api, account) => { // function called when API ready
-		api.csgo.prices("AWP | Dragon Lore (Factory New)", (data) => {
-			console.log(`DLore Price: $${data.median_price}`);
+		api.csgo.prices("AWP | Dragon Lore (Factory New)", (err, data) => {
+			if(!err) {
+				console.log(`DLore Price: $${data.median_price}`);
+			}
 		});
 	});
 
@@ -23,10 +24,27 @@ Which, at the time of writing, would output:
 
 	DLore Price: $1252.50
 
-Shitty Documentation
----------------------
-	steamlytics.csgo.account(callback) // Gets account information associated with the API key.
-	steamlytics.csgo.pricelist(callback, currency) // Gets the /v2/pricelist, see the Steamlytics api reference for currency details. The default is "$". The callback argument is the items array, or an empty array if your API level is not sufficient.
-	steamlytics.csgo.prices(market_hash_name, callback, options) // Gets the price of an item, options is an object that may contain parameters for the call. See the API reference for details.
-	steamlytics.csgo.items(callback) // Gets the list of items in the database. Callback arguments should be (num_items, items[]). See API reference for details.
-	steamlytics.csgo.popular(callback, limit) // Gets popular item list. The callback argument is the array of items
+## Documentation
+All function callbacks are formatted as `callback(error, data)`, where `error` is either null (if there's no error) or a throwable instance of `SteamlyticsError` which contains a message about what went wrong with the API call.
+
+### Function reference
+
+	steamlytics.csgo.account(callback)
+
+Gets account information associated with the API key.
+
+	steamlytics.csgo.pricelist([currency], callback)
+	
+Gets the /v2/pricelist, see the Steamlytics for more details. If you do not supply a currency, it will default to USD. The callback argument is the items array, or an empty array if your API level is not sufficient or an error occurs.
+
+	steamlytics.csgo.prices(market_hash_name, [options], callback)
+
+Gets the price of an item, options is optional object that may contain parameters for the call. See the API reference for details.
+
+	steamlytics.csgo.items(callback)
+
+Gets the list of items in the database. Data returned is in the format {num_items, items[]}. See API reference for details.
+
+	steamlytics.csgo.popular([limit], callback)
+
+Gets popular item list. The callback argument is the array of items. Limit is an optional parameter that limits the data returned.
--- steamlytics.js
+++ steamlytics.js
@@ -11,12 +11,14 @@ function getJSON(url, callback) {
 		if(resp.headers.location) {
 			getJSON(resp.headers.location, callback);
 		} else {
+			var callback_arg = {};
 			try {
 				var d_parsed = JSON.parse(data);
-				callback(d_parsed);
+				callback_arg = d_parsed;
 			} catch(e) {
-				callback({success: false, message: "There was an error parsing the response from steamlytics, their servers may be down.", exception: e});
+				callback_arg = {success: false, message: "There was an error parsing the response from steamlytics, their servers may be down.", exception: e};
 			}
+			callback(callback_arg);
 		}
 	});
 }
@@ -35,14 +37,14 @@ function Steamlytics(apiKey, readyCallback) {
 
 	/**
 	 * Gets the account information associated with this API key.
-	 * @param {function({api_plan:number, subscription_ends_at:number, calls_this_minute:number, calls_today:number})} callback The function to be called when the api call is complete.
+	 * @param {function(SteamlyticsError, Object)} callback The function to be called when the api call is complete.
 	 */
 	csgo.account = (callback) => {
 		getJSON(`http://api.steamlytics.xyz/v1/account?key=${this.apiKey}`, (data) => {
 			if(data.success) {
-				callback(data);
+				callback(null, data);
 			} else {
-				throw new SteamlyticsError(data.message);
+				callback(new SteamlyticsError(data.message), {});
 			}
 		})
 	}
@@ -50,12 +52,13 @@ function Steamlytics(apiKey, readyCallback) {
 	/**
 	 * Gets the pro-plan enabled pricelist (V2). If the account tied to the API key does not have pro-plan or higher, this will return an empty array.
 	 * 
-	 * @param {function({name:string, safe_price:string, safe_net_price: string, ongoing_price_manipulation: boolean, total_volume: number, "7_days":{median_price: string, median_net_price: string, average_price: string, average_net_price: string, lowest_price: string, lowest_net_price: string, highest_price: string, highest_net_price: string, mean_absolute_deviation: string, deviation_percentage: number, trend: number, volume: number}, "30_days":{median_price: string, median_net_price: string, average_price: string, average_net_price: string, lowest_price: string, lowest_net_price: string, highest_price: string, highest_net_price: string, mean_absolute_deviation: string, deviation_percentage: number, trend: number, volume: number}, "all_time":{median_price: string, median_net_price: string, average_price: string, average_net_price: string, lowest_price: string, lowest_net_price: string, highest_price: string, highest_net_price: string, mean_absolute_deviation: string, deviation_percentage: number, trend: number, volume: number}, first_seen: number}[])} callback The function to be called when the api call is complete.
 	 * @param {string} [currency="$"] The currency code used for the api request. The default is 2001 (USD or $). View the list of valid currencies at http://steam.steamlytics.xyz/currencies#currencies
+	 * @param {function(SteamlyticsError, Array<Object>)} callback The function to be called when the api call is complete.
 	 */
-	csgo.pricelist = (callback, currency) => {
-		if(!currency) {
+	csgo.pricelist = (currency, callback) => {
+		if(typeof currency == 'function') {
 			var currencyURL = ``;
+			callback = currency;
 		} else {
 			var currencyURL = `&currency=${currency}`;
 		}
@@ -64,10 +67,9 @@ function Steamlytics(apiKey, readyCallback) {
 		}
 		getJSON(`http://api.csgo.steamlytics.xyz/v2/pricelist?key=${this.apiKey}${currencyURL}`, (data) => {
 			if(data.success) {
-				callback(data.items);
+				callback(null, data.items);
 			} else {
-				console.error(data.message);
-				callback([]);
+				callback(new SteamlyticsError(data.message), []);
 			}
 		});
 	}
@@ -76,79 +78,82 @@ function Steamlytics(apiKey, readyCallback) {
 	 * Gets the price of a single item.
 	 * 
 	 * @param {string} market_hash_name The market_hash_name of the item.
-	 * @param {function({success: boolean, median_price: string, median_net_price: string, average_price: string, average_net_price: string, lowest_price: string, lowest_net_price: string, highest_price: string, highest_net_price: string, mean_absolute_deviation: string, deviation_percentage: number, volume: number, first_seen: number})} callback the function to be called when the api call is complete. The only parameter should be an array of items. See the API docs at http://csgo.steamlytics.xyz/api for more details.
 	 * @param {object} [options] the additional parameters for the api call. See the API docs at http://csgo.steamlytics.xyz/api for more details. 
+	 * @param {function(SteamlyticsError, Array<Object>)} callback the function to be called when the api call is complete. See the API docs at http://csgo.steamlytics.xyz/api for more details.
 	 */
-	csgo.prices = (market_hash_name, callback, options) => {
-		if(!options) {
-			options = {};
-		}
+	csgo.prices = (market_hash_name, options, callback) => {
 		var options_str = "";
-		if(options.from) {
-			options_str += `&from=${options.from}`;
-		}
-		if(options.to) {
-			options_str += `&to=${options.to}`;
-		}
-		if(options.on) {
-				if(options.from || options.to) {
-				throw new SteamlyticsError('May not use "from" or "to" in conjunction with "on".');
+		if(typeof options == 'function') {
+			callback = options;
+		} else {
+			if(options.from) {
+				options_str += `&from=${options.from}`;
 			}
-			options_str += `&on=${options.on}`;
-		}
-		if(options.currency && this.apiLevel >= 2) {
-			options_str += `&currency=${options.currency}`;
+			if(options.to) {
+				options_str += `&to=${options.to}`;
+			}
+			if(options.on) {
+					if(options.from || options.to) {
+					throw new SteamlyticsError('May not use "from" or "to" in conjunction with "on".');
+				}
+				options_str += `&on=${options.on}`;
+			}
+			if(options.currency && this.apiLevel >= 2) {
+				options_str += `&currency=${options.currency}`;
+			}
 		}
 		getJSON(`http://api.csgo.steamlytics.xyz/v1/prices/${market_hash_name}?key=${this.apiKey}${options_str}`, (data) => {
-			if(!data.success) {
-				console.error(data.message);
+			if(data.success) {
+				callback(null, data);
+			} else {
+				callback(new SteamlyticsError(data.message), {});
 			}
-			callback(data);
 		});
 	}
 	/**
 	 * Gets a list of all items currently tracked in the Steamlytics database.
 	 * 
-	 * @param {function(number, {market_name: string, market_hash_name: string, icon_url: string, name_color: string, quality_color: string}[])} callback the function to be called when the api call is complete. See the api docs at http://csgo.steamlytics.xyz/api for more details.
+	 * @param {function(SteamlyticsError, {num_items:number, items:Object[]})} callback the function to be called when the api call is complete. See the api docs at http://csgo.steamlytics.xyz/api for more details.
 	 */
 	csgo.items = (callback) => {
 		getJSON(`http://api.csgo.steamlytics.xyz/v1/items?key=${this.apiKey}`, (data) => {
 			if(data.success) {
-				callback(data.num_items, data.items);
+				callback(null, {num_items: data.num_items, items:data.items});
 			} else {
-				console.log(data.message);
-				callback(0, []);
+				callback(new SteamlyticsError(data.message), {num_items: 0, items: []});
 			}
 		});
 	}
 	/**
 	 * Gets the current list of popular items
 	 * 
-	 * @param {function({rank: number, market_hash_name: string, volume: number})} callback The callback for when the API call is complete. See the api docs at http://csgo.steamlytics.xyz/api for more details.
 	 * @param {number} [limit] (optional) The limit for how many results to return
+	 * @param {function(SteamlyticsError, {rank: number, market_hash_name: string, volume: number}[])} callback The callback for when the API call is complete. See the api docs at http://csgo.steamlytics.xyz/api for more details.
 	 */
-	var popular = (callback, limit) => {
-		if(!limit) {
-			limit = 0;
-		}
-		if(limit > 0) {
-			var limit_str = `&limit=${limit}`;
+	var popular = (limit, callback) => {
+		var limit_str = ``;
+		if(typeof limit == 'function') {
+			callback = limit;
 		} else {
-			var limit_str = ``;
+			limit_str = `&limit=${limit}`;
 		}
 		getJSON(`http://api.csgo.steamlytics.xyz/v1/items/popular?key=${this.apiKey}${limit_str}`, (data) => {
 			if(data.success) {
-				callback(data.items);
+				callback(null, data.items);
 			} else {
-				console.error(data.message);
-				callback([]);
+				callback(new SteamlyticsError(data.message), []);
 			}
 		});
 	}
-	csgo.account((data) => {
-		this.apiLevel = data.api_plan;
-		this.ready = true;
-		readyCallback(this, data);
+
+	csgo.account((err, data) => {
+		if(!err) {
+			this.apiLevel = data.api_plan;
+			this.ready = true;
+			readyCallback(this, data);
+		} else {
+			throw err;
+		}
 	})
 
 	this.csgo = csgo;
